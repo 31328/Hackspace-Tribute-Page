85c46de4a53a71e44b5475a53b286c0065f3e449
31328 <you@example.com>
2018-03-13 22:34:47 +0000

    Fix HTML formatting and fix bugs in JS.

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ var listOfNames = [
   "Andrew Pye"
 ];
 
-var thankyou = [
+var thankyous = [
   "Thank You",
   "AFRIKAANS – dankie",
   "ALBANIAN – faleminderit",
@@ -22,20 +22,92 @@ var thankyou = [
   "CROATIAN – hvala (HVAH-lah)",
   "CZECH – děkuji (Dyekooyih)",
   "DANISH – tak (tahg)",
-  "DUTCH – dank u", 'ESTONIAN – tänan (TA-nahn)', 'FINNISH – kiitos (KEE-tohss)', 'FRENCH – merci', 'GERMAN – danke', 'GREEK – ευχαριστώ (ef-hah-rees-TOH)', 'HAWAIIAN – mahalo (ma-HA-lo)', 'HEBREW – .תודה  / todah (toh-DAH)', 'HINDI – dhanyavād / shukriya', 'HUNGARIAN – köszönöm (KØ-sø-nøm)', 'ICELANDIC – takk (tahk) ', 'INDONESIAN – terima kasih. (tuh-REE-mah KAH-see)', 'ITALIAN – grazie (GRAHT-tsyeh)', 'JAPANESE – arigatô (ah-ree-GAH-toh)', 'KOREAN – 감사합니다 (gamsahamnida)', 'LATVIAN – paldies (PUHL-dyehs)', 'LEBANESE – choukrane', 'LITHUANIAN – ačiū (AH-choo)', 'Skopijan– Благодарам / blagodaram (blah-GOH-dah-rahm)', 'MALAY – terima kasih (TREE-muh KAH-seh)', 'MALTESE – grazzi (GRUTS-ee)', 'MANDARIN – 谢谢 Xièxiè', 'MONGOLIAN – Баярлалаа (bayarlalaa)', 'NORWEGIAN – takk', 'POLISH – dziękuję (Jenkoo-yen)', 'PORTUGUESE – obrigado', 'ROMANIAN – mulţumesc (mool-tzoo-MESK)', 'RUSSIAN – спасибо (spuh-SEE-buh)', 'SERBIAN – xвала / hvala (HVAH-lah)', 'SLOVAK – Ďakujem (JAH-koo-yehm)', 'SLOVENIAN – hvala (HVAA-lah)', 'SPANISH – gracias (GRAH-syahs)', 'SWEDISH – tack', 'TAMIL – nandri', 'THAI – kop khun', 'TURKISH – teşekkür ederim (teh shek uer eh der eem)', 'UKRAINIAN – Дякую (DYAH-koo-yoo)', 'WELSH – diolch (DEE-ol’ch)', 'YIDDISH – a dank', 'ZULU – ngiyabonga'
-];
+  "DUTCH – dank u",
+  'ESTONIAN – tänan (TA-nahn)',
+  'FINNISH – kiitos (KEE-tohss)',
+  'FRENCH – merci', 'GERMAN – danke',
+  'GREEK – ευχαριστώ (ef-hah-rees-TOH)',
+  'HAWAIIAN – mahalo (ma-HA-lo)',
+  'HEBREW – .תודה  / todah (toh-DAH)',
+  'HINDI – dhanyavād / shukriya',
+  'HUNGARIAN – köszönöm (KØ-sø-nøm)',
+  'ICELANDIC – takk (tahk) ',
+  'INDONESIAN – terima kasih. (tuh-REE-mah KAH-see)',
+  'ITALIAN – grazie (GRAHT-tsyeh)',
+  'JAPANESE – arigatô (ah-ree-GAH-toh)',
+  'KOREAN – 감사합니다 (gamsahamnida)',
+  'LATVIAN – paldies (PUHL-dyehs)',
+  'LEBANESE – choukrane', 'LITHUANIAN – ačiū (AH-choo)',
+  'Skopijan– Благодарам / blagodaram (blah-GOH-dah-rahm)',
+  'MALAY – terima kasih (TREE-muh KAH-seh)',
+  'MALTESE – grazzi (GRUTS-ee)',
+  'MANDARIN – 谢谢 Xièxiè',
+  'MONGOLIAN – Баярлалаа (bayarlalaa)',
+  'NORWEGIAN – takk',
+  'POLISH – dziękuję (Jenkoo-yen)',
+  'PORTUGUESE – obrigado',
+  'ROMANIAN – mulţumesc (mool-tzoo-MESK)',
+  'RUSSIAN – спасибо (spuh-SEE-buh)',
+  'SERBIAN – xвала / hvala (HVAH-lah)',
+  'SLOVAK – Ďakujem (JAH-koo-yehm)',
+  'SLOVENIAN – hvala (HVAA-lah)',
+  'SPANISH – gracias (GRAH-syahs)',
+  'SWEDISH – tack', 'TAMIL – nandri',
+  'THAI – kop khun',
+  'TURKISH – teşekkür ederim (teh shek uer eh der eem)',
+  'UKRAINIAN – Дякую (DYAH-koo-yoo)',
+  'WELSH – diolch (DEE-ol’ch)',
+  'YIDDISH – a dank',
+  'ZULU – ngiyabonga'
+]
 
-function thankyou(name) {
+var rejections = [
+  "Are you sure you are a founding memeber or fellow of the Hackspace",
+  "Why are you insist?",
+  "Come ooon, give it a rest!",
+  "Ok, that's enought...",
+  "Just let the page alone and don't push your luck",
+  "Why are you doing this?",
+  "Haven't you had enough yet?",
+  "You need to take 'NO' as an answer",
+  "I'm not going to 'thank' random people, no way!",
+  "Well, since you insist I'll think about it...",
+  "You truly are persistent!",
+  "Just let me think what you can do to say 'thank you'",
+  "Would you like to make a donation?",
+  "If yes, then get in touch with us!",
+  "If you do you I'll add you to the 'thank you list' - promise...",
+  "No other way - donation first!",
+]
+
+var indexRejection = 0;
+
+function thankyou() {
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
+
+  // Is the name in the array listOfNames?
+
+  //yes
   if (listOfNames.indexOf(name) !== -1) {
-    var index = (Math.random() * hellos.length) | 0;
+    var index = (Math.random() * thankyous.length) | 0;
     var thankyou = thankyous[index];
 
-    console.log(thankyous + " " + name + "!");
+    alert(thankyou + " " + name + "!");
+
+    //no  
   } else {
-    console.log(
-      "Are you sure you are a founding member or fellow of the Preston Hackspace???"
-    );
+
+    // Reset to 0 if we get to end of the array
+    if (indexRejection > rejections.length - 1) {
+      indexRejection = 0;
+    }
+
+    var rejection = rejections[indexRejection];
+
+    alert(rejection + " " + name + "!");
+
+    // Increment by 1
+    indexRejection++;
   }
 }
-
-sayHello("Chris Dell");
